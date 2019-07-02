ab4e8c633556ac7706e236dc02e850b48b48d936
sounishnath003 <flock.sinasini@gmail.com>
2019-07-02 22:32:19 +0530

    some refractoring

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as  http;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -7,25 +10,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+void _pushSaved() {}
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: _myAppBar()
+      appBar: new AppBar(
+      title: new Text("CryptoList"),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 3.0,
+      actions: <Widget>[
+        new IconButton(
+          icon: Icon(
+            Icons.list,
+            size: 30.0,
+          ),
+          onPressed: _pushSaved,
+        ),
+        new SizedBox(
+          width: 16.0,
+        )
+      ]),
+      body: AppBody(),
     );
   }
 }
 
-AppBar _myAppBar() {
-  return new AppBar(
-     title: new Text("CryptoList"),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 3.0,
-          actions: <Widget>[
-            new Icon(Icons.list,
-            size: 30.0,
-            ),
-            new SizedBox(width: 16.0,)
-          ]
-  );
+
+class AppBody extends StatefulWidget {
+  AppBody({Key key}) : super(key: key);
+
+  _AppBodyState createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+List _cryptoList ;
+
+Future<void> getCryptoPrices() async {
+  print("getting: crypto prices") ;
+  String _apiUrl = "https://api.coinmarketcap.com/v1/ticker/";
+  http.Response response = await http.get(_apiUrl);
+
+  if(response.statusCode == 200) {
+    jsonDecode(response.body);
+  } else {
+    throw("Error Unexpectedly Occurs...");
+  }
+
+  setState(() {
+      this._cryptoList =
+          jsonDecode(response.body); //sets the state of our widget
+      print(_cryptoList); //prints the list
+    });
+    return ;
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Expanded(
+
+        )
+    );
+  }
 }
