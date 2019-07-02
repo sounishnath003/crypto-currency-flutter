3562f715415c28977300a02daa2b12edf16d4b4f
sounishnath003 <flock.sinasini@gmail.com>
2019-07-02 15:51:05 +0530

    appbar widget

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
 
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("CryptoList"),
+      appBar: _myAppBar()
+    );
+  }
+}
+
+AppBar _myAppBar() {
+  return new AppBar(
+     title: new Text("CryptoList"),
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 3.0,
@@ -16,8 +26,6 @@ class MyHomePage extends StatelessWidget {
             size: 30.0,
             ),
             new SizedBox(width: 16.0,)
-          ],
-      ),
-    );
-  }
+          ]
+  );
 }
